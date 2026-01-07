6f5d4221348fc74eebf4389745dddf264ab358dd
Anbu <priyaramcd82@gmail.com>
2026-01-07 18:18:28 +0530

    Changes for swagger

---
+++ src/main/java/com/springboot/orders/exception/PaymentFailedException.java
@@ -0,0 +1,7 @@
+package com.springboot.orders.exception;
+
+public class PaymentFailedException extends RuntimeException{
+    public PaymentFailedException(String message) {
+        super(message);
+    }
+}
